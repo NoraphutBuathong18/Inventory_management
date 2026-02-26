16e3f49502913bf7b266907d67268e5e05687ddc
NB18 <146542664+NoraphutBuathong18@users.noreply.github.com>
2026-02-26 22:23:39 +0700

    update backend api url

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_BASE_URL || 'https://inventory-management-fj7b.onrender.com/api',
+    baseURL: import.meta.env.VITE_API_URL || 'https://inventory-management-fj7b.onrender.com/api',
     headers: { 'Content-Type': 'application/json' },
 })
 
